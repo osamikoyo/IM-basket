716ca8d59a908a6aa2628e382b61a0a6121f054c
osamikoyo <v.pylayev@mail.ru>
2025-02-22 08:32:13 +0500

    add config and data

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,2 +1,32 @@
 package config
 
+import (
+	"io"
+	"os"
+
+	"gopkg.in/yaml.v3"
+)
+
+type Config struct{
+	AmqpUrl string `yaml:"amqp_url"`
+	MongoUrl string `yaml:"mongo_url"`
+	Port uint32
+	Host string
+}
+
+func Load(path string) (*Config, error) {
+	file, err := os.Open(path)
+	if err != nil{
+		return nil, err
+	}
+
+	body, err := io.ReadAll(file)
+	if err != nil{
+		return nil, err
+	}
+
+	var cfg Config
+
+	err = yaml.Unmarshal(body, &cfg)
+	return &cfg, err
+}
--- internal/data/data.go
+++ internal/data/data.go
@@ -1,2 +1,32 @@
 package data
 
+import (
+	"context"
+	"time"
+
+	"github.com/osamikoyo/IM-basket/internal/config"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+type Data struct{
+	coll *mongo.Collection
+	ctx context.Context
+}
+
+func New(cfg config.Config) (*Data, error) {
+	url := options.Client().ApplyURI(cfg.MongoUrl)
+	client, err := mongo.NewClient(url)
+	if err != mongo.ErrNilCursor{
+		return nil, err
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 30 * time.Second)
+	defer cancel()
+
+	coll := client.Database("im").Collection("basket")
+	return &Data{
+		coll: coll,
+		ctx: ctx,
+	}, nil
+}
