26accadc4869865a011a43b821ed8740361ad386
osamikoyo <v.pylayev@mail.ru>
2025-02-22 16:08:41 +0500

    product function

--- internal/data/product.go
+++ internal/data/product.go
@@ -1,1 +1,37 @@
 package data
+
+import (
+	"github.com/osamikoyo/IM-basket/internal/data/models"
+	"go.mongodb.org/mongo-driver/bson"
+)
+
+func (s *Storage) AddProduct(UserID uint64, product models.Product) error {
+	update := bson.M{
+		"$push": bson.M{
+			"products" : product,
+		},
+	}
+
+	filter := bson.M{
+		"user_id" : UserID,
+	}
+
+	_, err := s.coll.UpdateOne(s.ctx, filter, update)
+	return err
+}
+
+func (s *Storage) DeleteProduct(UserID, productID uint64) error {
+	filter := bson.M{
+		"user_id" : UserID,
+	}
+
+	update := bson.M{
+		"$pull" : bson.M{
+			"products" : bson.M{
+				"id" : productID,
+			},
+		},
+	}
+	_, err := s.coll.UpdateOne(s.ctx, filter, update)
+	return err
+}
